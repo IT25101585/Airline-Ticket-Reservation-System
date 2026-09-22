77da3290277038b1dedd36bafcca0445196319e4
Nihinsa2005 <tnh.withana@gmail.com>
2026-09-22 19:48:22 +0530

    Reset repository contents

--- README.md
+++
@@ -1,2 +0,0 @@
-# Airline-Ticket-Reservation-System
- 2026-Y2-S1-MLB-B6G1-03 
--- backend/payment-management/src/main/java/com/example/demopay/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.example.demopay;
-
-public class Launcher {
-    public static void main(String[] args) {
-        PaymentApplication.main(args);
-    }
-}
